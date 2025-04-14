ffc53ac41ee1fa3e5650853073f0c8442a7392db
Chia-Hsiang Tsai <tsaichiahsiang2000@gmail.com>
2025-04-14 16:04:34 +0800

    temp the jax, will modify in the future

--- utils.py
+++ utils.py
@@ -1,11 +1,7 @@
 import numpy as np
 from typing import Literal
 import torch
-try:
-    import jax.numpy as jnp
-    import jaxlib
-except:
-    pass
+
 
 
 FRAMEWORK_DTYPE_MAP = {
@@ -22,27 +18,43 @@ FRAMEWORK_DTYPE_MAP = {
         'float':torch.float32,
         'double':torch.float64,
         'bool':torch.bool
-    },
-    'jnp':{
+    }
+}
+try:
+    import jax.numpy as jnp
+    import jaxlib
+    FRAMEWORK_DTYPE_MAP['jnp']={
         'int':jnp.int32,
         'long':jnp.int16,
         'float':jnp.float32,
         'double':jnp.float64,
         'bool':jnp.bool
     }
-}
+    def as_type(arr:np.ndarray|torch.Tensor|jnp.ndarray, dtype:Literal['int','long', 'float', 'double', 'bool'])->np.ndarray|torch.Tensor|jnp.ndarray:
 
-def as_type(arr:np.ndarray|torch.Tensor|jnp.ndarray, dtype:Literal['int','long', 'float', 'double', 'bool'])->np.ndarray|torch.Tensor|jnp.ndarray:
+        match type(arr):
+            case np.ndarray:
+                return arr.astype(dtype=FRAMEWORK_DTYPE_MAP['np'][dtype])
+            case torch.Tensor:
+                return arr.to(dtype=FRAMEWORK_DTYPE_MAP['torch'][dtype])
+            case jnp.ndarray:
+                return arr.astype(dtype=FRAMEWORK_DTYPE_MAP['jnp'][dtype])
+            case jaxlib.xla_extension.ArrayImpl:
+                return arr.astype(dtype=FRAMEWORK_DTYPE_MAP['jnp'][dtype])
+            case _:
+                raise NotImplementedError(f"{type(arr)} is not support.")
+
+except:
+    pass
+
+def as_type(arr:np.ndarray|torch.Tensor, dtype:Literal['int','long', 'float', 'double', 'bool'])->np.ndarray|torch.Tensor:
 
     match type(arr):
         case np.ndarray:
             return arr.astype(dtype=FRAMEWORK_DTYPE_MAP['np'][dtype])
         case torch.Tensor:
             return arr.to(dtype=FRAMEWORK_DTYPE_MAP['torch'][dtype])
-        case jnp.ndarray:
-            return arr.astype(dtype=FRAMEWORK_DTYPE_MAP['jnp'][dtype])
-        case jaxlib.xla_extension.ArrayImpl:
-            return arr.astype(dtype=FRAMEWORK_DTYPE_MAP['jnp'][dtype])
         case _:
             raise NotImplementedError(f"{type(arr)} is not support.")
+
 
