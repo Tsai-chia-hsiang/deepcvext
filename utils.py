f8c2a550e6aa3a847677c6f67bd5bfc95e947f68
Tsai-chia-hsiang <tsaichiahsiang2000@gmail.com>
2025-05-10 18:44:46 +0800

    change the function name from color_cvt to cvtcolor to align opencv's naming style

--- utils.py
+++ utils.py
@@ -2,50 +2,8 @@ import numpy as np
 from typing import Literal, Optional
 import torch
 import cv2
+from . import FRAMEWORK_DTYPE_MAP
 
-
-FRAMEWORK_DTYPE_MAP = {
-    'np':{
-        'int':np.int32,
-        'long':np.int64,
-        'float':np.float32,
-        'double':np.float64,
-        'bool':np.bool
-    },
-    'torch':{
-        'int':torch.int32,
-        'long':torch.long,
-        'float':torch.float32,
-        'double':torch.float64,
-        'bool':torch.bool
-    }
-}
-try:
-    import jax.numpy as jnp
-    import jaxlib
-    FRAMEWORK_DTYPE_MAP['jnp']={
-        'int':jnp.int32,
-        'long':jnp.int16,
-        'float':jnp.float32,
-        'double':jnp.float64,
-        'bool':jnp.bool
-    }
-    def as_type(arr:np.ndarray|torch.Tensor|jnp.ndarray, dtype:Literal['int','long', 'float', 'double', 'bool'])->np.ndarray|torch.Tensor|jnp.ndarray:
-
-        match type(arr):
-            case np.ndarray:
-                return arr.astype(dtype=FRAMEWORK_DTYPE_MAP['np'][dtype])
-            case torch.Tensor:
-                return arr.to(dtype=FRAMEWORK_DTYPE_MAP['torch'][dtype])
-            case jnp.ndarray:
-                return arr.astype(dtype=FRAMEWORK_DTYPE_MAP['jnp'][dtype])
-            case jaxlib.xla_extension.ArrayImpl:
-                return arr.astype(dtype=FRAMEWORK_DTYPE_MAP['jnp'][dtype])
-            case _:
-                raise NotImplementedError(f"{type(arr)} is not support.")
-
-except:
-    pass
 
 def as_type(arr:np.ndarray|torch.Tensor, dtype:Literal['int','long', 'float', 'double', 'bool'])->np.ndarray|torch.Tensor:
 
@@ -66,7 +24,7 @@ _cvtflag = (
     (None, cv2.COLOR_BGRA2GRAY, None, cv2.COLOR_BGRA2BGR, None) # src: bgra
 )
 
-def color_cvt(img:np.ndarray, to_channel:Optional[int]=None):
+def cvtcolor(img:np.ndarray, to_channel:Optional[int]=None):
     global _cvtflag
     i = img.copy().astype(np.uint8)
     if img.ndim == 2:
