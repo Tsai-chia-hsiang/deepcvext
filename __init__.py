09d97a6b559889f758206c0f9dae26ca00482697
Tsai-chia-hsiang <tsaichiahsiang2000@gmail.com>
2025-05-10 18:45:18 +0800

    move DTYPE_MAP here

--- __init__.py
+++ __init__.py
@@ -1,1 +1,21 @@
+import numpy as np 
+import torch
 from .convert import *
+
+FRAMEWORK_DTYPE_MAP = {
+    'np':{
+        'int':np.int32,
+        'long':np.int64,
+        'float':np.float32,
+        'double':np.float64,
+        'bool':np.bool
+    },
+    'torch':{
+        'int':torch.int32,
+        'long':torch.long,
+        'float':torch.float32,
+        'double':torch.float64,
+        'bool':torch.bool
+    }
+}
+
