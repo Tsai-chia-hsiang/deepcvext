5e60a4802b238729908cac8a15acecb1a1ef5c15
Tsai-chia-hsiang <tsaichiahsiang2000@gmail.com>
2025-05-18 16:31:59 +0800

    move dtype utils to dtypes.py

--- __init__.py
+++ __init__.py
@@ -2,22 +2,6 @@ import numpy as np
 import torch
 from .convert import *
 
-DTYPE_MAP = {
-    'np':{
-        'int':np.int32,
-        'long':np.int64,
-        'float':np.float32,
-        'double':np.float64,
-        'bool':np.bool_
-    },
-    'torch':{
-        'int':torch.int32,
-        'long':torch.long,
-        'float':torch.float32,
-        'double':torch.float64,
-        'bool':torch.bool
-    }
-}
 
 AXIS_MAP = {
     np.ndarray:{
